22fc47af0e8c4e4bd7829ab62fd273ccc311e52b
saitoyoshi <mitti180@gmail.com>
2023-06-23 19:14:01 +0900

    編集画面のデザイン完成

--- resources/views/components/tweet/options.blade.php
+++ resources/views/components/tweet/options.blade.php
@@ -1,22 +1,22 @@
 @once
 @push('css')
-    <style>
-        .tweet-option > summary {
-            list-style: none;
-            cursor: pointer;
-        }
-        .tweet-option[open] > summary::before {
-            position: fixed;
-            top: 0;
-            right: 0;
-            bottom: 0;
-            left: 0;
-            z-index: 10;
-            display: block;
-            content: "";
-            background:transparent;
-        }
-    </style>
+<style>
+    .tweet-option > summary {
+        list-style: none;
+        cursor: pointer;
+    }
+    .tweet-option[open] > summary::after {
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: -10;
+        display: block;
+        content: " ";
+        background: transparent;
+    }
+</style>
 
 @endpush
 @endonce
@@ -30,16 +30,15 @@
     <div class="bg-white rounded shadow-md absolute right-0 w-24 z-20 pt-1 pb-1">
         <div>
             <a href="{{ route('tweet.update.index', ['tweetId' => $tweetId]) }}" class="flex items-center pt-1 pb-1 pl-3 pr-3 hover:bg-gray-100">
-
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-                <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
-            </svg>
-            <span>編集</span>
-        </a>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                    <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
+                </svg>
+                <span>編集</span>
+            </a>
         </div>
         <div>
-            <form action="{{ route('tweet.delete', ['tweetId' => $tweetId]) }}" method="post" onclick="return confirm('削除してもよろしいですか？')">
+            <form action="{{ route('tweet.delete', ['tweetId' => $tweetId]) }}" method="post" onclick="return confirm('削除してもよろしいですか?');">
                 @method('DELETE')
                 @csrf
                 <button type="submit" class="flex items-center w-full pt-1 pb-1 pl-3 pr-3 hover:bg-gray-100">
